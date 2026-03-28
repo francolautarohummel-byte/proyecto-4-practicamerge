854ca95c6d4b5c1483c0f3bc6fcac3c8ae8539d9
Franco <francolautarohummel@gmail.com>
2026-03-27 21:10:08 -0300

    Se realizó otra función para script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function interaccion1(){
     alert("Interacción 1");
 }
+
+function interaccion2(){
+    let print = "";
+    for(let i = 0; i <= 10; i++){
+        print += i;
+    }
+    alert(print);
+}
